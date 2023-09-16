db5c412d59b7264dc76ee89e7617365fdc3e2520
Puneeth G Koushik <puneeth.koushik2@gmail.com>
2023-09-17 01:44:52 +0530

    Added day 5 exercises and virtual env files are added in screenshots in jpg

---
+++ Python_practice_exercises/module.py
@@ -0,0 +1,11 @@
+def func_():
+    return "Hey hello from Mphasis from module."
+
+custom_variable = "Hey hello from Mphasis"
+
+class CustomClass:
+    def __init__(self):
+        self.message = "Custom Class Instance from my_module"
+
+    def print_message(self):
+        print(self.message)
